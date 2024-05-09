7c7d852730f6621b9ba50b0fb213e2bd43f0e324
MassimoRomano <romano93m@gmail.com>
2024-05-09 15:22:49 +0200

    add style for welcome page

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>welcome page</h1>
+    <div class="row align-items-md-stretch">
+        <div class="h-100 p-5 text-white bg-dark text-center">
+            <h2 class="text-danger">WELCOME TO MOVIES LIST</h2>
+            <p class="py-4">
+                Swap the background-color utility and add a `.text-*` color
+                utility to mix up the jumbotron look. Then, mix and match with
+                additional component themes and more.
+            </p>
+            <div class="d-flex justify-content-center gap-4">
+                <button class="btn btn-outline-success" type="button">
+                    Register
+                </button>
+                <button class="btn btn-outline-primary" type="button">
+                    Sign In
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <section class="bg-dark">
+        <div class="container py-5">
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-3 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <h4 class="card-title">{{ $movie->title }}</h4>
+                                    <h6 class="card-subtitle text-muted">{{ $movie->original_title }}</h6>
+                                </div>
+                                <p class="card-text">{{ $movie->nationality }}</p>
+                                <p class="card-text">{{ $movie->date }}</p>
+                                <p class="card-text">{{ $movie->vote }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
