f1e309333f264783934cc6a04108c3bb952a6304
MassimoRomano <romano93m@gmail.com>
2024-05-09 14:53:15 +0200

    add function for pagecontroller and route for web

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    
+    public function index()
+    {
+
+        $movies = Movie::orderByDesc('id')->get();
+
+        return view('guests.welcome', compact('movies'));
+    }
+
+    public function about()
+    {
+        return view('guests.about');
+    }
+
+    public function contacts()
+    {
+        return view('guests.contacts');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guests\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [PageController::class,'index'])->name('welcome');
+Route::get("/about", [PageController::class,"about"])->name('about');
+Route::get('', [PageController::class,'contacts'])->name('contacts');
